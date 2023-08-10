66da8c9aea97199b70813c680218026f3c72ad0a
Daniel Díaz <diaz_carrete@yahoo.com>
2023-08-10 04:09:09 +0200

    name changes

--- lib/Cauldron.hs
+++ lib/Cauldron.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE GADTs #-}
 {-# LANGUAGE OverloadedRecordDot #-}
 
 module Cauldron
@@ -21,6 +20,7 @@ import Algebra.Graph.AdjacencyMap (AdjacencyMap)
 import Algebra.Graph.AdjacencyMap qualified as Graph
 import Algebra.Graph.AdjacencyMap.Algorithm qualified as Graph
 import Algebra.Graph.Export.Dot qualified as Dot
+import Data.Bifunctor (first)
 import Data.ByteString qualified
 import Data.Dynamic
 import Data.Foldable
@@ -32,117 +32,144 @@ import Data.Map.Strict qualified as Map
 import Data.Maybe (fromJust)
 import Data.SOP (All, K (..))
 import Data.SOP.NP
+import Data.Set (Set)
+import Data.Set qualified as Set
 import Data.Text qualified
 import Data.Text.Encoding qualified
 import Data.Typeable
 import Multicurryable
 
-newtype Cauldron = Cauldron {recipes :: Map TypeRep Constructor}
+data Cauldron = Cauldron
+  { recipes :: Map TypeRep Recipe,
+    recipesConflicts :: Set TypeRep
+  }
 
 empty :: Cauldron
-empty = Cauldron do Map.empty
+empty = Cauldron {recipes = Map.empty, recipesConflicts = Set.empty}
 
+-- | Might throw a 'ConflictingBeanRecipes' exception.
 put ::
-  forall (as :: [Type]) (b :: Type) curried.
+  forall (as :: [Type]) (b :: Type) recipe.
   ( All Typeable as,
     Typeable b,
-    MulticurryableF as b curried (IsFunction curried)
+    MulticurryableF as b recipe (IsFunction recipe)
   ) =>
-  curried ->
+  recipe ->
   Cauldron ->
   Cauldron
-put curried Cauldron {recipes} =
-  Cauldron do
-    Map.insert
-      do typeRep (Proxy @b)
-      do makeConstructor @as @b @curried curried
-      recipes
+put recipe Cauldron {recipes, recipesConflicts} =
+  let rep = typeRep (Proxy @b)
+   in Cauldron
+        { recipes =
+            Map.insert
+              rep
+              do makeRecipe @as @b @recipe recipe
+              recipes,
+          recipesConflicts =
+            case Map.lookup rep recipes of
+              Nothing -> recipesConflicts
+              _ -> Set.insert rep recipesConflicts
+        }
 
-data Constructor where
-  Constructor ::
+data Recipe where
+  Recipe ::
     (All Typeable as, Typeable b) =>
-    { argumentReps :: [TypeRep],
-      resultRep :: TypeRep,
-      uncurried :: NP I as -> b
+    { ingredientReps :: [TypeRep],
+      beanRep :: TypeRep,
+      unrecipe :: NP I as -> b
     } ->
-    Constructor
+    Recipe
 
-makeConstructor ::
-  forall (as :: [Type]) (b :: Type) curried.
+makeRecipe ::
+  forall (as :: [Type]) (b :: Type) recipe.
   ( All Typeable as,
     Typeable b,
-    MulticurryableF as b curried (IsFunction curried)
+    MulticurryableF as b recipe (IsFunction recipe)
   ) =>
-  curried ->
-  Constructor
-makeConstructor curried =
-  Constructor
-    { argumentReps =
+  recipe ->
+  Recipe
+makeRecipe recipe =
+  Recipe
+    { ingredientReps =
         collapse_NP do
           cpure_NP @_ @as
             do Proxy @Typeable
             typeRepHelper,
-      resultRep = typeRep (Proxy @b),
-      uncurried = multiuncurry @(->) @as @b @curried curried
+      beanRep = typeRep (Proxy @b),
+      unrecipe = multiuncurry @(->) @as @b @recipe recipe
     }
   where
     typeRepHelper :: forall a. (Typeable a) => K TypeRep a
     typeRepHelper = K do typeRep (Proxy @a)
 
+type Plan = [TypeRep]
+
 cook :: Cauldron -> Either Mishap Beans
-cook Cauldron {recipes} =
-  case findMissingDeps do (.argumentReps) <$> recipes of
-    missing | Data.Foldable.any (not . Data.List.null) missing ->
-      Left do MissingDeps missing
-    _ ->
-      let depGraph =
+cook Cauldron {recipes, recipesConflicts} = do
+  () <- first ConflictingRecipes checkConflicts
+  () <- first MissingDeps checkMissing
+  (recipeGraph, plan) <- first RecipeCycle checkCycles
+  let beans = build plan
+  Right Beans {recipeGraph, beans}
+  where
+    checkConflicts :: Either (Set TypeRep) ()
+    checkConflicts =
+      if not do Set.null recipesConflicts
+        then Left do recipesConflicts
+        else Right ()
+    checkMissing :: Either (Map TypeRep [TypeRep]) ()
+    checkMissing =
+      case Map.map
+        do Prelude.filter (`Map.notMember` recipes)
+        do (.ingredientReps) <$> recipes of
+        missing | Data.Foldable.any (not . Data.List.null) missing ->
+          Left do missing
+        _ ->
+          Right ()
+    checkCycles :: Either (Graph.Cycle TypeRep) (AdjacencyMap TypeRep, Plan)
+    checkCycles =
+      let recipeGraph =
             Graph.edges
               do
                 flip
                   Map.foldMapWithKey
                   recipes
-                  \resultRep Constructor {argumentReps} -> do
-                    argumentRep <- argumentReps
-                    [(resultRep, argumentRep)]
-       in case Graph.topSort depGraph of
+                  \beanRep Recipe {ingredientReps} -> do
+                    ingredientRep <- ingredientReps
+                    [(beanRep, ingredientRep)]
+       in case Graph.topSort recipeGraph of
             Left depCycle ->
-              Left do DepCycle depCycle
-            Right buildPlan ->
-              let beans =
-                    Data.List.foldl'
-                      do
-                        \dynMap rep ->
-                          let constructor = fromJust do Map.lookup rep recipes
-                              dyn = construct dynMap constructor
-                           in Map.insert (dynTypeRep dyn) dyn dynMap
-                      Map.empty
-                      buildPlan
-               in Right Beans {depGraph, beans}
+              Left depCycle
+            Right plan -> Right (recipeGraph, plan)
+    build :: Plan -> Map TypeRep Dynamic
+    build =
+      Data.List.foldl'
+        do
+          \dynMap rep ->
+            let recipe = fromJust do Map.lookup rep recipes
+                dyn = followRecipe dynMap recipe
+             in Map.insert (dynTypeRep dyn) dyn dynMap
+        Map.empty
 
 data Mishap
-  = MissingDeps (Map TypeRep [TypeRep])
-  | DepCycle (NonEmpty TypeRep)
+  = ConflictingRecipes (Set TypeRep)
+  | MissingDeps (Map TypeRep [TypeRep])
+  | RecipeCycle (NonEmpty TypeRep)
   deriving stock (Show)
 
 data Beans = Beans
-  { depGraph :: AdjacencyMap TypeRep,
+  { recipeGraph :: AdjacencyMap TypeRep,
     beans :: Map TypeRep Dynamic
   }
 
-findMissingDeps :: Map TypeRep [TypeRep] -> Map TypeRep [TypeRep]
-findMissingDeps theMap =
-  Map.map
-    do Prelude.filter (`Map.notMember` theMap)
-    theMap
-
--- | Build a component out of already build components.
+-- | Build a bean out of already built beans.
 -- This can only work without blowing up if there aren't dependecy cycles
 -- and the order of construction respects the depedencies!
-construct :: Map TypeRep Dynamic -> Constructor -> Dynamic
-construct theDyns Constructor {uncurried} =
+followRecipe :: Map TypeRep Dynamic -> Recipe -> Dynamic
+followRecipe theDyns Recipe {unrecipe} =
   let argsExtractor = sequence_NP do cpure_NP (Proxy @Typeable) makeExtractor
       args = runExtractor argsExtractor theDyns
-   in toDyn do uncurried args
+   in toDyn do unrecipe args
 
 newtype Extractor a = Extractor {runExtractor :: Map TypeRep Dynamic -> a}
   deriving newtype (Functor, Applicative)
@@ -154,15 +181,16 @@ makeExtractor =
         fromJust do fromDynamic @a do fromJust do Map.lookup rep dyns
    in Extractor {runExtractor}
 
+-- | Might throw an error if the bean is not present!
 taste :: forall a. (Typeable a) => Beans -> a
 taste Beans {beans} = runExtractor makeExtractor beans
 
 exportToDot :: FilePath -> Beans -> IO ()
-exportToDot filepath Beans {depGraph} = do
+exportToDot filepath Beans {recipeGraph} = do
   let prettyRep rep =
         Data.Text.pack do tyConName do typeRepTyCon rep
       dot =
         Dot.export
           do Dot.defaultStyle prettyRep
-          depGraph
+          recipeGraph
   Data.ByteString.writeFile filepath (Data.Text.Encoding.encodeUtf8 dot)
