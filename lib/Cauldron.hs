fe5b77b426cab29cb25f4633f80a0fafecb82212
Daniel Díaz <diaz_carrete@yahoo.com>
2023-09-17 10:45:13 +0200

    Sharper internal types

--- lib/Cauldron.hs
+++ lib/Cauldron.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ViewPatterns #-}
 
 module Cauldron
   ( Cauldron,
@@ -31,13 +31,15 @@ import Algebra.Graph.Export.Dot qualified as Dot
 import Data.Bifunctor (first)
 import Data.ByteString qualified
 import Data.Dynamic
+import Data.Foldable qualified
+import Data.Functor.Identity
 import Data.Kind
 import Data.List qualified
 import Data.List.NonEmpty (NonEmpty)
 import Data.Map.Strict (Map)
 import Data.Map.Strict qualified as Map
 import Data.Maybe (fromJust)
-import Data.SOP (All, K (..), And)
+import Data.SOP (All, K (..))
 import Data.SOP.NP
 import Data.Set (Set)
 import Data.Set qualified as Set
@@ -45,9 +47,8 @@ import Data.Text qualified
 import Data.Text.Encoding qualified
 import Data.Typeable
 import Multicurryable
-import Data.Foldable qualified
 
-newtype Cauldron = Cauldron { recipes :: Map TypeRep Recipe }
+newtype Cauldron = Cauldron {recipes :: Map TypeRep (Recipe_ Maybe)}
 
 empty :: Cauldron
 empty = Cauldron {recipes = Map.empty}
@@ -66,43 +67,46 @@ insert recipe Cauldron {recipes} =
         { recipes =
             Map.insert
               rep
-              do Recipe { 
-                  beanCon = Just do Constructor @args @bean recipe,
-                  decosCons = []
-              }
+              do
+                Recipe
+                  { beanCon = Just do Constructor @args @bean recipe,
+                    decosCons = []
+                  }
               recipes
         }
 
-data Recipe where
-  Recipe :: { 
-    beanCon :: Maybe Constructor, 
-    decosCons :: [Constructor]
-  } -> Recipe
+data Recipe_ f where
+  Recipe ::
+    { beanCon :: f Constructor,
+      decosCons :: [Constructor]
+    } ->
+    Recipe_ f
+
+type Recipe = Recipe_ Identity
 
 data Constructor where
   Constructor ::
     (All Typeable args, Typeable bean) =>
-    { 
-      constructor :: Args args (Regs '[] bean)
+    { constructor :: Args args (Regs '[] bean)
     } ->
     Constructor
 
-data ConstructorReps = ConstructorReps {
-  argsReps :: [TypeRep],
-  resultRep :: TypeRep
-}
+data ConstructorReps = ConstructorReps
+  { argsReps :: [TypeRep],
+    resultRep :: TypeRep
+  }
 
 -- https://discord.com/channels/280033776820813825/280036215477239809/1147832555828162594
 -- https://github.com/ghc-proposals/ghc-proposals/pull/126#issuecomment-1363403330
 constructorReps :: Constructor -> ConstructorReps
-constructorReps Constructor {constructor = (_ :: Args args (Regs '[] result))} = 
+constructorReps Constructor {constructor = (_ :: Args args (Regs '[] result))} =
   ConstructorReps
     { argsReps =
         collapse_NP do
           cpure_NP @_ @args
             do Proxy @Typeable
             typeRepHelper,
-      resultRep = 
+      resultRep =
         typeRep (Proxy @result)
     }
   where
@@ -116,61 +120,75 @@ boil ::
   Cauldron ->
   Either Mishap (BeanGraph, Map TypeRep Dynamic)
 boil Cauldron {recipes} = do
-  () <- first BeanlessDecorator checkBeanlessDecos
-  () <- first MissingBeanDependencies checkMissingDeps
-  (beanGraph, plan) <- first ConstructorCycle checkCycles
-  let beans = build plan
+  recipes' <- first BeanlessDecorator do checkBeanlessDecos recipes
+  () <- first MissingBeanDependencies do checkMissingDeps recipes'
+  (beanGraph, plan) <- first ConstructorCycle do checkCycles recipes'
+  let beans = build recipes' plan
   Right (BeanGraph {beanGraph}, beans)
-  where
-    checkBeanlessDecos :: Either (Set TypeRep) ()
-    checkBeanlessDecos =
-        case flip Map.foldMapWithKey
-                recipes 
-                do \beanRep -> \case 
-                      Recipe { beanCon = Just _} -> Set.empty
-                      _ -> Set.singleton beanRep
-        of 
-        missing | not do Data.List.null missing ->
+
+checkBeanlessDecos ::
+  Map TypeRep (Recipe_ Maybe) ->
+  Either (Set TypeRep) (Map TypeRep Recipe)
+checkBeanlessDecos recipes =
+  case flip
+    Map.foldMapWithKey
+    recipes
+    do
+      \beanRep -> \case
+        recipe@Recipe {beanCon = Just con} -> (Set.empty, Map.singleton beanRep (recipe {beanCon = Identity con}))
+        _ -> (Set.singleton beanRep, Map.empty) of
+    (missing, _)
+      | not do Data.List.null missing ->
           Left missing
-        _ -> 
-          Right ()
-    checkMissingDeps :: Either (Map TypeRep [TypeRep]) ()
-    checkMissingDeps =
-      case Map.map
-        do Prelude.filter (`Map.notMember` recipes)
-        do (.argsReps) . constructorReps . fromJust . (.beanCon) <$> recipes 
-      of
-      missing | Data.Foldable.any (not . Data.List.null) missing ->
-        Left missing
-      _ ->
-        Right ()
-    checkCycles :: Either (Graph.Cycle TypeRep) (AdjacencyMap TypeRep, Plan)
-    checkCycles =
-      let beanGraph =
-            Graph.edges
-              do
-                flip
-                  Map.foldMapWithKey
-                  recipes
-                  \beanRep (constructorReps . fromJust . (.beanCon) -> ConstructorReps {argsReps}) -> do
-                    argRep <- argsReps
-                    [(beanRep, argRep)]
-       in case Graph.topSort beanGraph of
-            Left recipeCycle ->
-              Left recipeCycle
-            Right plan -> Right (beanGraph, plan)
-    build :: Plan -> Map TypeRep Dynamic
-    build =
-      Data.List.foldl'
-        do
-          \dynMap rep ->
-            let constructor = fromJust do (.beanCon) do fromJust do Map.lookup rep recipes
-                dyn = followConstructor dynMap constructor
-             in Map.insert (dynTypeRep dyn) dyn dynMap
-        Map.empty
+    (_, result) ->
+      Right result
+
+checkMissingDeps ::
+  Map TypeRep Recipe ->
+  Either (Map TypeRep [TypeRep]) ()
+checkMissingDeps recipes =
+  case Map.map
+    do Prelude.filter (`Map.notMember` recipes)
+    do (.argsReps) . constructorReps . runIdentity . (.beanCon) <$> recipes of
+    missing
+      | Data.Foldable.any (not . Data.List.null) missing ->
+          Left missing
+    _ ->
+      Right ()
+
+checkCycles ::
+  Map TypeRep Recipe ->
+  Either (Graph.Cycle TypeRep) (AdjacencyMap TypeRep, Plan)
+checkCycles recipes = do
+  let beanGraph =
+        Graph.edges
+          do
+            flip
+              Map.foldMapWithKey
+              recipes
+              \beanRep (constructorReps . runIdentity . (.beanCon) -> ConstructorReps {argsReps}) -> do
+                argRep <- argsReps
+                [(beanRep, argRep)]
+  case Graph.topSort beanGraph of
+    Left recipeCycle ->
+      Left recipeCycle
+    Right plan -> Right (beanGraph, plan)
+
+build ::
+  Map TypeRep Recipe ->
+  Plan ->
+  Map TypeRep Dynamic
+build recipes =
+  Data.List.foldl'
+    do
+      \dynMap rep ->
+        let constructor = runIdentity do (.beanCon) do fromJust do Map.lookup rep recipes
+            dyn = followConstructor dynMap constructor
+         in Map.insert (dynTypeRep dyn) dyn dynMap
+    Map.empty
 
 data Mishap
-  = BeanlessDecorator (Set TypeRep) 
+  = BeanlessDecorator (Set TypeRep)
   | MissingBeanDependencies (Map TypeRep [TypeRep])
   | ConstructorCycle (NonEmpty TypeRep)
   deriving stock (Show)
@@ -190,7 +208,7 @@ followConstructor theDyns Constructor {constructor} = do
 newtype Extractor a = Extractor {runExtractor :: Map TypeRep Dynamic -> a}
   deriving newtype (Functor, Applicative)
 
-makeExtractor :: forall a. Typeable a => Extractor a
+makeExtractor :: forall a. (Typeable a) => Extractor a
 makeExtractor =
   let runExtractor dyns =
         fromJust do taste (Proxy @a) dyns
@@ -206,27 +224,27 @@ exportToDot filepath BeanGraph {beanGraph} = do
           beanGraph
   Data.ByteString.writeFile filepath (Data.Text.Encoding.encodeUtf8 dot)
 
-taste :: forall a. Typeable a => Proxy a -> Map TypeRep Dynamic -> Maybe a
+taste :: forall a. (Typeable a) => Proxy a -> Map TypeRep Dynamic -> Maybe a
 taste _ dyns = do
   let rep = typeRep (Proxy @a)
   dyn <- Map.lookup rep dyns
   fromDynamic @a dyn
 
-newtype Args args r = 
-  Args { runArgs :: NP I args -> r }
+newtype Args args r = Args {runArgs :: NP I args -> r}
   deriving newtype (Functor, Applicative, Monad)
 
 args0 :: r -> Args '[] r
 args0 r = Args do \_ -> r
 
-argsN :: forall (args :: [Type]) r curried.
-    MulticurryableF args r curried (IsFunction curried) =>
-    curried -> Args args r
+argsN ::
+  forall (args :: [Type]) r curried.
+  (MulticurryableF args r curried (IsFunction curried)) =>
+  curried ->
+  Args args r
 argsN = Args . multiuncurry
 
-newtype Regs (regs :: [Type]) r = 
-  Regs { runRegs :: (NP I regs, r) }
-  deriving newtype Functor
+newtype Regs (regs :: [Type]) r = Regs {runRegs :: (NP I regs, r)}
+  deriving newtype (Functor)
 
 regs0 :: r -> Regs '[] r
 regs0 r = Regs (Nil, r)
