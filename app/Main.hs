3bcf104f252c826617a08e2c3cffbdf3e740221d
Daniel Díaz <diaz_carrete@yahoo.com>
2023-11-11 07:29:01 +0100

    comments

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DerivingStrategies #-}
 
 -- | We have a bunch of datatypes, and a single recipe (constructor) for each
 -- datatype. This means the wiring can be type-directed: we don't have to make a
@@ -73,6 +74,23 @@ data Y = Y deriving (Show)
 data Z = Z deriving (Show)
 
 {- 
+  These beans are a bit special: they are "secondary" beans which are optionally
+  produced by the constructors of other beans.
+  
+  They have Monoid instances. The values returned by all the constructors that
+  produce them will be combined.
+-}
+
+-- | A window into the internal state of some bean.
+newtype Inspector = Inspector { inspect :: IO [String] }
+    deriving newtype (Semigroup, Monoid)
+
+-- | And initialization action which some beans might register.
+newtype Initializer = Initializer { runInitializer :: IO () }
+    deriving newtype (Semigroup, Monoid)
+
+
+{- 
   HERE ARE A BUNCH OF CONSTRUCTORS AND DECORATORS.
 
 -}
@@ -83,8 +101,8 @@ makeA = A
 -- 
 -- The registration could be some generic introspection mechanism, or perhaps
 -- some effectful action that sets up a worker thread.
-makeB :: (Sum Int, B)
-makeB = (Sum 1, B)
+makeB :: (Inspector, B)
+makeB = (Inspector (pure ["B stuff"]), B)
 
 makeC :: C
 makeC = C
@@ -92,16 +110,16 @@ makeC = C
 makeD :: D
 makeD = D
 
--- | A bean with an effectful initialization.
+-- | A bean with an effectful constructor.
 --
 -- We might want this in order to allocate some internal IORef,
 -- or perhaps to read some configuration file.
 makeE :: IO (A -> E)
 makeE = pure \_ -> E
 
--- | A bean with an effectful initialization and a monoidal registration.
-makeF :: IO (B -> C -> (Sum Int, F))
-makeF = pure \_ _ -> (Sum 1,F)
+-- | A bean with an effectful constructor and a monoidal registration.
+makeF :: IO (B -> C -> (Inspector, F))
+makeF = pure \_ _ -> (Inspector (pure ["F stuff"]),F)
 
 -- | A bean with a self-dependency! 
 --
@@ -112,23 +130,37 @@ makeG :: E -> F -> G -> G
 makeG _ _ _ = G
 
 -- | A decorator.
+--
+--  Decorators are basically normal constructors, only that they return 
+--  a Endo that knows how to tweak the value of a bean.
+--
+-- Because they are normal constructors, they can be effectful, and they
+-- might have dependencies of their own.
 makeGDeco1 :: A -> Endo G
 makeGDeco1 _ = mempty
 
-makeH :: A -> D -> G -> (Sum Int, H)
-makeH _ _ _ = (Sum 1, H)
+-- | A bean with two monoidal registrations.
+makeH :: A -> D -> G -> (Initializer, Inspector, H)
+makeH _ _ _ = (Initializer (putStrLn "H init"), Inspector (pure ["H stuff"]), H)
 
-makeZ :: Sum Int -> D -> H -> Z
+-- | Notice that this bean has "Inspector" as a dependency. Inspector is a
+-- monoidal bean which is aggregated across all the constructor that register
+-- it. This is OK as long as there are no dependency cycles. 
+-- 
+-- Why would a bean depend on such a aggregated bean? Well, for example, a
+-- server bean might want to publish diagnostic information collected from beans
+-- that register it.
+makeZ :: Inspector -> D -> H -> Z
 makeZ _ _ _ = Z
 
 makeZDeco1 :: B -> E -> Endo Z
 makeZDeco1 _ _ = mempty
 
--- | A decorator with effectful initialization and a monoidal registration.
-makeZDeco2 :: IO (F -> (Sum Int, Endo Z))
-makeZDeco2 = pure \_ -> (Sum 1, mempty)
+-- | A decorator with an effectful constructor and a monoidal registration.
+makeZDeco2 :: IO (F -> (Initializer, Endo Z))
+makeZDeco2 = pure \_ -> (Initializer (putStrLn "Z deco init"), mempty)
 
-boringWiring :: IO (Sum Int, Z)
+boringWiring :: IO (Initializer, Inspector, Z)
 boringWiring = do
   -- We need to run the effectful constructors first.
   makeE' <- makeE
@@ -136,30 +168,32 @@ boringWiring = do
   makeZDeco2' <- makeZDeco2
   let 
       -- We have to remember to collect the monoidal registrations. 
-      reg = reg1 <> reg2 <> reg3 <> reg4
+      initializer = init1 <> init2
+      -- We have to remember to collect the monoidal registrations. 
+      inspector = inspector1 <> inspector2 <> inspector3
       -- Now let's tie the constructors together.
       a = makeA
-      (reg1, b) = makeB
+      (inspector1, b) = makeB
       c = makeC
       d = makeD
       e = makeE' a
-      (reg2, f) = makeF' b c
+      (inspector2, f) = makeF' b c
       gDeco1 = makeGDeco1 a
       -- Here we apply a single decorator.
       g = appEndo gDeco1 do makeG e f g
       zDeco1 = makeZDeco1 b e
-      (reg3, zDeco2) = makeZDeco2' f
-      (reg4, h) = makeH a d g
+      (init1, zDeco2) = makeZDeco2' f
+      (init2, inspector3, h) = makeH a d g
       -- Compose the decorators before applying them.
-      z = appEndo (zDeco2 <> zDeco1) do makeZ reg d h
-  pure (reg, z)
+      z = appEndo (zDeco2 <> zDeco1) do makeZ inspector d h
+  pure (initializer, inspector , z)
 
--- | Here we don't have to worry about positional parameters. We simply throw
--- all the constructors into the 'Cauldron' and taste the bean values at the
--- end, plus a graph we may want to draw.
---
--- Note that we detect wiring errors before running the initialization.
-coolWiring :: Either BadBeans (BeanGraph, IO (Maybe (Sum Int, Z)))
+-- | Here we don't have to worry about positional parameters. We throw all the
+-- constructors into the 'Cauldron' and taste the bean values at the end, plus a
+-- graph we may want to draw.
+-- 
+-- Note that we detect wiring errors *before* running the effectful constructors.
+coolWiring :: Either BadBeans (BeanGraph, IO (Maybe (Initializer,Inspector,Z)))
 coolWiring =
   let cauldron :: Cauldron IO =
         foldr
@@ -179,7 +213,7 @@ coolWiring =
                       [ pack_ do pure makeGDeco1
                       ]
                 },
-            insert @H do bare do pack (\(reg, bean) -> regs1 reg bean) do pure makeH,
+            insert @H do bare do pack (\(reg1, reg2, bean) -> regs2 reg1 reg2 bean) do pure makeH,
             insert @Z do
               Bean
                 { constructor = pack_ do pure makeZ,
@@ -196,19 +230,29 @@ coolWiring =
           Right (beanGraph, do
             beans <- action
             pure do 
-              reg <- taste @(Sum Int) beans
+              initializer <- taste @Initializer beans
+              inspector <- taste @Inspector beans
               z <- taste @Z beans
-              pure (reg, z)
+              pure (initializer, inspector, z)
             )
 
 main :: IO ()
 main = do
-  do wiring <- boringWiring
-     print wiring
+  do (Initializer {runInitializer} , Inspector {inspect}, z) <- boringWiring
+     inspection <- inspect
+     print inspection
+     print z
+     runInitializer
   case coolWiring of
-    Left mishap -> do
-      print mishap
+    Left badBeans -> do
+      print badBeans
     Right (beanGraph, action) -> do
-      result <- action
-      print result
       exportToDot "beans.dot" beanGraph
+      result <- action
+      case result of 
+        Nothing -> print "oops"
+        Just (Initializer {runInitializer}, Inspector {inspect}, z) -> do
+          inspection <- inspect
+          print inspection
+          print z
+          runInitializer
