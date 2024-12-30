5185fdbc5f8627e0ddeb076f197ecc64a1889b61
Daniel Díaz <diaz_carrete@yahoo.com>
2024-12-30 09:28:11 +0100

    MonadBuilder typeclass

--- lib/Cauldron/Builder.hs
+++ lib/Cauldron/Builder.hs
@@ -1,52 +1,25 @@
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE FunctionalDependencies #-}
-module Cauldron.Builder where
+
+module Cauldron.Builder
+  ( Builder,
+    add,
+    MonadBuilder (..),
+  )
+where
 
 import Cauldron
-import Cauldron.Args (Args)
-import Algebra.Graph.AdjacencyMap (AdjacencyMap)
-import Algebra.Graph.AdjacencyMap qualified as Graph
-import Algebra.Graph.AdjacencyMap.Algorithm qualified as Graph
-import Algebra.Graph.Export.Dot qualified as Dot
 import Cauldron.Args
-import Cauldron.Beans (SomeMonoidTypeRep (..))
-import Cauldron.Beans qualified
-import Control.Exception (Exception (..))
-import Control.Monad.Fix
-import Data.Bifunctor (first)
-import Data.ByteString qualified
+import Cauldron.Managed
 import Data.Dynamic
-import Data.Foldable qualified
 import Data.Function ((&))
-import Data.Functor ((<&>))
-import Data.Functor.Identity (Identity (..))
+import Data.Functor.Identity
 import Data.Kind
-import Data.List qualified
-import Data.List.NonEmpty (NonEmpty)
-import Data.List.NonEmpty qualified
-import Data.Map.Strict (Map)
-import Data.Map.Strict qualified as Map
-import Data.Maybe (fromJust)
-import Data.Semigroup qualified
-import Data.Sequence (Seq)
-import Data.Sequence qualified
-import Data.Set (Set)
-import Data.Set qualified as Set
-import Data.Text qualified
-import Data.Text.Encoding qualified
-import Data.Tree
-import Data.Type.Equality (testEquality)
-import Data.Typeable
-import GHC.Exception (CallStack, prettyCallStackLines)
-import GHC.IsList
-import GHC.Stack (HasCallStack, callStack, withFrozenCallStack)
-import Type.Reflection qualified
-
 
 data Builder m a = Builder (Cauldron m) a
-    deriving stock Functor
+  deriving stock (Functor)
 
 instance Applicative (Builder m) where
   pure a = Builder Cauldron.empty a
@@ -64,14 +37,34 @@ add ::
   -- | A 'Recipe' or a 'Constructor'.
   recipelike m bean ->
   Builder m (Args bean)
-add recipelike = Builder (Cauldron.empty & Cauldron.insert  recipelike) (arg @bean)
+add recipelike = Builder (Cauldron.empty & Cauldron.insert recipelike) (arg @bean)
 
-class MonadBuilder m b | b -> m where
-    type ArgsWrapper b :: Type -> Type
-    addVal :: Typeable a => ArgsWrapper b a -> m (ArgsWrapper b a)
-    addEff :: Typeable a => ArgsWrapper b (b a) -> m (ArgsWrapper b a)
+class (Applicative (ArgsWrapper b), Monad (ConstructorEff b)) => MonadBuilder b where
+  type ArgsWrapper b :: Type -> Type
+  type ConstructorEff b :: Type -> Type
+  addVal :: (Typeable a) => ArgsWrapper b a -> b (ArgsWrapper b a)
+  addEff :: (Typeable a) => ArgsWrapper b (ConstructorEff b a) -> b (ArgsWrapper b a)
 
-instance (Monad m, Applicative (ArgsWrapper (Builder m))) => MonadBuilder m (Builder m) where
-    type ArgsWrapper (Builder m) = Args
-    addVal v = add (val_ v) 
-    addEff action = add (eff_ action) 
+instance (Monad m) => MonadBuilder (Builder m) where
+  type ArgsWrapper (Builder m) = Args
+  type ConstructorEff (Builder m) = m
+  addVal :: (Monad m, Typeable a) => Args a -> Builder m (Args a)
+  addVal v = add (val_ v)
+  addEff :: (Monad m, Typeable a) => Args (m a) -> Builder m (Args a)
+  addEff action = add (eff_ action)
+
+instance MonadBuilder IO where
+  type ArgsWrapper IO = Identity
+  type ConstructorEff IO = IO
+  addVal :: (Typeable a) => Identity a -> IO (Identity a)
+  addVal = pure
+  addEff :: (Typeable a) => Identity (IO a) -> IO (Identity a)
+  addEff = sequence
+
+instance MonadBuilder Managed where
+  type ArgsWrapper Managed = Identity
+  type ConstructorEff Managed = Managed
+  addVal :: (Typeable a) => Identity a -> Managed (Identity a)
+  addVal = pure
+  addEff :: (Typeable a) => Identity (Managed a) -> Managed (Identity a)
+  addEff = sequence
