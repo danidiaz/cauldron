6fe4ab839e72dea15f996f1a147eea414b277f24
Daniel Díaz <diaz_carrete@yahoo.com>
2025-05-17 11:40:50 +0200

    findCycleInSCC

--- lib-graph/Cauldron/Graph/Algorithm.hs
+++ lib-graph/Cauldron/Graph/Algorithm.hs
@@ -8,47 +8,41 @@ import Cauldron.Graph
 import Data.List.NonEmpty
 import Data.Graph qualified
 import Data.Foldable (for_)
-import Data.Sequence (Seq)
 import Data.Sequence qualified
-import Data.List qualified
-import Data.Set (Set)
 import Data.Set qualified
-import Data.Map (Map)
 import Data.Map.Strict qualified
+import Data.Foldable qualified
 
 reverseTopSort :: Ord a => AdjacencyMap a -> Either (NonEmpty a) [a] 
-reverseTopSort am = do
+reverseTopSort g = do
     let theEdges = do
-            (i,o) <- adjacencyList am 
+            (i,o) <- adjacencyList g 
             [(i,i,o)]
         sccs = Data.Graph.stronglyConnComp theEdges
     for_ sccs $ \case
         Data.Graph.AcyclicSCC _  -> pure ()
-        Data.Graph.NECyclicSCC vs -> 
-            do 
-                let aCycle = findCycleInSCC (adjacencyMap am) vs
+        Data.Graph.NECyclicSCC vs -> do
+                let aCycle = findCycleInSCC g vs
                 Left aCycle
-    let (g,nodeFromVertex,_) = Data.Graph.graphFromEdges theEdges
+    let (g',nodeFromVertex,_) = Data.Graph.graphFromEdges theEdges
     Right $ do
-            ves <- Data.Graph.reverseTopSort g
+            ves <- Data.Graph.reverseTopSort g'
             let (v,_,_) = nodeFromVertex ves
             [v]
 
 findCycleInSCC :: Ord a => AdjacencyMap a -> NonEmpty a -> NonEmpty a
-findCycleInSCC g scc@(start :| _) = go start [start] (Data.Set.singleton start)
+findCycleInSCC g scc@(start :| _) = go start (Data.Set.singleton start) (Data.Sequence.singleton start)
   where
-    sccSet = Data.Set.fromList . toList $ scc
+    sccSet = Data.Set.fromList . Data.Foldable.toList $ scc
     isInScc = (`Data.Set.member` sccSet)
     am = adjacencyMap $ Cauldron.Graph.induce isInScc g
-    go current acc visited =
+    go current visited cycleAcc =
       case Data.Set.toList <$> Data.Map.Strict.lookup current am of
         Nothing -> error "findCycleInSCC: node not in adjacency map"
         Just [] -> error "findCycleInSCC: SCC node with no outgoing edge"
         Just (child:_) ->
           if child `Data.Set.member` visited
             then
-              -- Prune acc up to the first occurrence of child, then return the cycle
-              let (_before, aCycle) = Data.List.break (== child) (Data.List.reverse acc)
-              in Data.List.NonEmpty.fromList  aCycle
+              Data.List.NonEmpty.fromList $ Data.Foldable.toList $ Data.Sequence.takeWhileL (/= child) cycleAcc
             else
-              go child (child:acc) (Data.Set.insert child visited)
+              go child (Data.Set.insert child visited) (cycleAcc Data.Sequence.|> child)
